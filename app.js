d36cba70459d6812ba9e228ea11dc401db06d84c
Alejandro Salgado <alejandrosram@outlook.com>
2022-08-10 20:19:32 -0500

    Update base

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 const { createApp } = Vue;
 
-const app = createApp({
-	template: `
-		<h3>{{ quote }}</h3>
-		<blockquote>- {{ author }}</blockquote>
+const quotes = [
+	{ quote: "The night is darkest just before the dawn. And I promise you, the dawn is coming.", author: "Harvey Dent, The Dark Knight" },
+	{ quote: "I believe what doesn’t kill you simply makes you, stranger.", author: "The Joker, The Dark Knight" },
+	{ quote: "Your anger gives you great power. But if you let it, it will destroy you… As it almost did me", author: "Henri Ducard, Batman Begins" },
+	{ quote: "You either die a hero or live long enough to see yourself become the villain.", author: "Harvey Dent, The Dark Knight" },
+	{ quote: "If you’re good at something, never do it for free.", author: "The Joker, The Dark Knight" },
+	{ quote: "Yes, father. I shall become a bat.", author: "Bruce Wayne/Batman, Batman: Year One" },
+];
 
-		<button v-on:click="changeQuote">
-			Change quote
-		</button>
-	`,
+const app = createApp({
 	data: () => ({
-		quote: "I'm Batman",
-		author: "Bruce Wayne",
+		quotes,
 	}),
-	methods: {
-		changeQuote() {
-			this.capitalize();
-			this.author = "Alex";
-		},
-		capitalize() {
-			this.quote = this.quote.toUpperCase();
-		},
-	},
 });
 
 app.mount("#root");
